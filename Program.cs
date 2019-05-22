9b5805a9057d7a15a608bebe3be8b288fcfda6c8
tomas962 <cctomass@gmail.com>
2019-05-22 22:13:23 +0300

    Initial commit (just data reading from file)

---
+++ Program.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUBGStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataFile = "../../Data/statsnocommas.csv";
+            List<List<double>> data = ReadDataAsList(dataFile, ';');
+            //WriteData(stats);
+            Stat a = new Stat();
+            Console.WriteLine();
+            Console.ReadKey();        
+        }
+
+        static List<Stat> ReadDataAsObjects(string fileName)
+        {
+            List<Stat> stats = new List<Stat>();
+            using (StreamReader reader = new StreamReader(fileName, Encoding.UTF8))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var param = line.Split(';');
+
+                    Stat stat = new Stat(param[0].ToString(), int.Parse(param[1]), double.Parse(param[2]), double.Parse(param[3]), double.Parse(param[4]), int.Parse(param[5]), int.Parse(param[6]),
+                        double.Parse(param[7]), int.Parse(param[8]), double.Parse(param[9]), int.Parse(param[10]), double.Parse(param[11]), double.Parse(param[12]), double.Parse(param[13]),
+                    double.Parse(param[14]), double.Parse(param[15]), double.Parse(param[16]), double.Parse(param[17]), double.Parse(param[18]), double.Parse(param[19]),
+                    double.Parse(param[20]), double.Parse(param[21]), int.Parse(param[22]), int.Parse(param[23]), int.Parse(param[24]), int.Parse(param[25]),
+                    int.Parse(param[26]), double.Parse(param[27]), int.Parse(param[28]), int.Parse(param[29]), int.Parse(param[30]), int.Parse(param[31]),
+                    int.Parse(param[32]), int.Parse(param[33]), int.Parse(param[34]), double.Parse(param[35]), double.Parse(param[36]), double.Parse(param[37]), int.Parse(param[38]),
+                    double.Parse(param[39]), double.Parse(param[40]), double.Parse(param[41]), double.Parse(param[42]), double.Parse(param[43]), double.Parse(param[44]),
+                    int.Parse(param[45]), int.Parse(param[46]), double.Parse(param[47]));
+
+                    stats.Add(stat);
+                }
+            }
+            return stats;
+        }
+
+        static List<List<double>> ReadDataAsList(string fileName, char separator)
+        {
+            List<List<double>> data = new List<List<double>>();
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string line;
+                string[] values;
+                int lineNr = 0;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    data.Add(new List<double>());
+                    
+                    values = line.Split(separator);
+                    //Skip first value, because it is string (player name)
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        data[lineNr].Add(double.Parse(values[i]));
+                    }
+                    lineNr++;
+                }
+            }
+            return data;
+        }
+
+        static void WriteData(List<Stat> stats)
+        {
+            foreach (var stat in stats)
+            {
+                Console.WriteLine(stat);
+            }
+        }
+
+    }
+}
---
+++ Stat.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUBGStatistics
+{
+    class Stat
+    {
+        public string PlayerName { get; set; }
+        public int TrackerID { get; set; }
+        public double KillDeathRatio { get; set; }
+        public double WinRatio { get; set; }
+        public double TimeSurvived { get; set; }
+        public int RoundsPlayed { get; set; }
+        public int Wins { get; set; }
+        public double WinTop10Ratio { get; set; }
+        public int Top10s { get; set; }
+        public double Top10Ratio { get; set; }
+        public int Losses { get; set; }
+        public double Rating { get; set; }
+        public double BestRating { get; set; }
+        public double DamagePg { get; set; }
+        public double HeadshotKillsPg { get; set; }
+        public double HealsPg { get; set; }
+        public double KillsPg { get; set; }
+        public double MoveDistancePg { get; set; }
+        public double RoadKillsPg { get; set; }
+        public double TeamKillsPg { get; set; }
+        public double TimeSurvivedPg { get; set; }
+        public double Top10sPg { get; set; }
+        public int Kills { get; set; }
+        public int Assists { get; set; }
+        public int Suicides { get; set; }
+        public int TeamKills { get; set; }
+        public int HeadshotKills { get; set; }
+        public double HeadshotKillRatio { get; set; }
+        public int VehicleDestroys { get; set; }
+        public int RoadKills { get; set; }
+        public int DailyKills { get; set; }
+        public int WeeklyKills { get; set; }
+        public int RoundMostKills { get; set; }
+        public int MaxKillStreaks { get; set; }
+        public int Days { get; set; }
+        public double LongestTimeSurvived { get; set; }
+        public double MostSurvivalTime { get; set; }
+        public double AvgSurvivalTime { get; set; }
+        public int WinPoints { get; set; }
+        public double WalkDistance { get; set; }
+        public double RideDistance { get; set; }
+        public double MoveDistance { get; set; }
+        public double AvgWalkDistance { get; set; }
+        public double AvgRideDistance { get; set; }
+        public double LongestKill { get; set; }
+        public int Heals { get; set; }
+        public int Boosts { get; set; }
+        public double DamageDealt { get; set; }
+
+        public Stat()
+        {
+        }
+        public Stat(string PlayerName, int TrackerID, double KillDeathRatio, double WinRatio, double TimeSurvived, int RoundsPlayed, int Wins, 
+         double WinTop10Ratio, int Top10s, double Top10Ratio, int Losses,
+         double Rating, double BestRating, double DamagePg, double HeadshotKillsPg, double HealsPg, double KillsPg,
+         double MoveDistancePg, double RoadKillsPg, double TeamKillsPg, double TimeSurvivedPg, double Top10sPg,
+         int Kills, int Assists, int Suicides, int TeamKills, int HeadshotKills,
+         double HeadshotKillRatio, int VehicleDestroys, int RoadKills, int DailyKills, int WeeklyKills,
+         int RoundMostKills, int MaxKillStreaks, int Days, double LongestTimeSurvived, double MostSurvivalTime,
+         double AvgSurvivalTime, int WinPoints, double WalkDistance, double RideDistance, double MoveDistance,
+         double AvgWalkDistance, double AvgRideDistance, double LongestKill, int Heals, int Boosts, double DamageDealt)
+        {
+            this.PlayerName = PlayerName;
+            this.TrackerID = TrackerID;
+            this.KillDeathRatio = KillDeathRatio;
+            this.WinRatio = WinRatio;
+            this.TimeSurvived = TimeSurvived;
+            this.RoundsPlayed = RoundsPlayed;
+            this.Wins = Wins;
+            this.WinTop10Ratio = WinTop10Ratio;
+            this.Top10s = Top10s;
+            this.Top10Ratio = Top10Ratio;
+            this.Losses = Losses;
+            this.Rating = Rating;
+            this.BestRating = BestRating;
+            this.DamagePg = DamagePg;
+            this.HeadshotKillsPg = HeadshotKillsPg;
+            this.HealsPg = HealsPg;
+            this.KillsPg = KillsPg;
+            this.MoveDistancePg = MoveDistancePg;
+            this.RoadKillsPg = RoadKillsPg;
+            this.TeamKillsPg = TeamKillsPg;
+            this.TimeSurvivedPg = TimeSurvivedPg;
+            this.Top10sPg = Top10sPg;
+            this.Kills = Kills;
+            this.Assists = Assists;
+            this.Suicides = Suicides;
+            this.TeamKills = TeamKills;
+            this.HeadshotKills = HeadshotKills;
+            this.HeadshotKillRatio = HeadshotKillRatio;
+            this.VehicleDestroys = VehicleDestroys;
+            this.RoadKills = RoadKills;
+            this.DailyKills = DailyKills;
+            this.WeeklyKills = WeeklyKills;
+            this.RoundMostKills = RoundMostKills;
+            this.MaxKillStreaks = MaxKillStreaks;
+            this.Days = Days;
+            this.LongestTimeSurvived = LongestTimeSurvived;
+            this.MostSurvivalTime = MostSurvivalTime;
+            this.AvgSurvivalTime = AvgSurvivalTime;
+            this.WinPoints = WinPoints;
+            this.WalkDistance = WalkDistance;
+            this.RideDistance = RideDistance;
+            this.MoveDistance = MoveDistance;
+            this.AvgWalkDistance = AvgWalkDistance;
+            this.AvgRideDistance = AvgRideDistance;
+            this.LongestKill = LongestKill;
+            this.Heals = Heals;
+            this.Boosts = Boosts;
+            this.DamageDealt = DamageDealt;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0,-15}|{1,-10}|{2,-10}|{3,-10}|{4,-10}|{5,-10}|{6,-10}|{7,-10}|{8,-10}|{9,-10}" +
+                "|{10,-10}|{11,-10}|{12,-10}|{13,-10}|{14,-10}|{15,-10}|{16,-10}|{17,-10}|{18,-10}|{19,-10}" +
+                "|{20,-10}|{21,-10}|{22,-10}|{23,-10}|{24,-10}|{25,-10}|{26,-10}|{27,-10}|{28,-10}|{29,-10}" +
+                "|{30,-10}|{31,-10}|{32,-10}|{33,-10}|{34,-10}|{35,-10}|{36,-10}|{37,-10}|{38,-10}|{39,-10}" +
+                "|{40,-10}|{41,-10}|{42,-10}|{43,-10}|{44,-10}|{45,-10}|{46,-10}|{47,-10}|", 
+                PlayerName, TrackerID, KillDeathRatio, WinRatio, TimeSurvived, RoundsPlayed, Wins,
+               WinTop10Ratio, Top10s, Top10Ratio, Losses,
+               Rating, BestRating, DamagePg, HeadshotKillsPg, HealsPg, KillsPg,
+               MoveDistancePg, RoadKillsPg, TeamKillsPg, TimeSurvivedPg, Top10sPg,
+               Kills, Assists, Suicides, TeamKills, HeadshotKills,
+               HeadshotKillRatio, VehicleDestroys, RoadKills, DailyKills, WeeklyKills,
+               RoundMostKills, MaxKillStreaks, Days, LongestTimeSurvived, MostSurvivalTime,
+               AvgSurvivalTime, WinPoints, WalkDistance, RideDistance, MoveDistance,
+               AvgWalkDistance, AvgRideDistance, LongestKill, Heals, Boosts, DamageDealt
+                ); ;
+        }
+    }
+
+}
